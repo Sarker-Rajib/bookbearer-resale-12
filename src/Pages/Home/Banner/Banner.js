7dcc105ccf8f5b41fcba38ef4bfdcb297e8e0fd3
Rajib Sarker <redhat0593@gmail.com>
2022-12-20 01:09:31 +0600

    banner updated

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import bannerImage from '../../../../src/Assets/Images/s.png';
+import '../Banner/Banner.css';
+// import bannerImage from '../../../../src/Assets/Images/s.png';
+// import bannerbg from '../../../../src/Assets/Images/banner-bg.jpg';
 
 const Banner = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-2 py-6'>
-            <div className="flex items-center">
-                <div>
-                    <h2 className='text-purple-600 text-4xl pb-2'>Welcome !</h2>
-                    <h3 className='text-5xl text-purple-700'>Buy and Sell Books <span className='text-green-600'>Online</span></h3>
-                    <p className='pt-4 text-xl'>Online platform for book seller and buyer for second hand books</p>
-                </div>
-            </div>
-            <div className="image">
-                <img src={bannerImage} alt="imag" className="img-fluid" />
+        <div className='banner-home flex items-center'>
+            <div className='text'>
+                <h2 className=' text-4xl pb-2'>Welcome !</h2>
+                <h3 className='text-5xl '>Buy and Sell Books <span>Online</span></h3>
+                <p className='pt-4 text-xl'>Online platform for book seller and buyer for second hand books</p>
             </div>
         </div>
     );
